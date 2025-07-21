b6abf0cd9a9fbc19e190c9c18e0d9c0a9b2e0969
Mohammed said <182983589+cubetech110@users.noreply.github.com>
2025-07-21 14:30:47 +0400

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
       {
         docuseal: {
           'color-scheme': 'light',
-          primary: '#e4e0e1',
-          secondary: '#ef9fbc',
-          accent: '#eeaf3a',
+          primary: '#8b8b8bff',
+          secondary: '#0033ffff',
+          accent: '#212121ff',
           neutral: '#291334',
           'base-100': '#faf7f5',
           'base-200': '#efeae6',
           'base-300': '#e7e2df',
-          'base-content': '#291334',
+          'base-content': '#8412bdff',
           '--rounded-btn': '1.9rem',
           '--tab-border': '2px',
           '--tab-radius': '.5rem'
